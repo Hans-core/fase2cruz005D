5c5ddf892d95da7b94a4f0f84cdea2e1bc764f2a
unknown <hans.cruz.0011@gmail.com>
2020-11-03 20:59:33 -0300

    creando sitio

--- criticasHC/peliculas/models.py
+++ criticasHC/peliculas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.urls import reverse 
+import uuid
 # Create your models here.
+class movie(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+	name = models.CharField (max_length=100)
+	enum_gen =(
+		('Accion','acc'),('Terror','terr'),('Horror','Horr'),('Comedia','Com'),('Ficción','fic'),('Otros','otro'),
+		)
+	gene = models.CharField (max_length=10, choices=enum_gen, blank=True, default='Otros')
+	desc = models.TextField (max_length=1000, blank=True, default='')
+	clasif = models.IntegerField ()
+	imagen = models.ImageField (upload_to='media/imagenes', null=True, blank=True, default='media/imagenes/not-found.png') 
+	def __str__(self):
+		f'{self.name},{self.gene},{self.desc},{self.clasif}'
