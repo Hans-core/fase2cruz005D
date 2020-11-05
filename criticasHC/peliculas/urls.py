880aeb40407721352053a70e73b1f5275be71d58
unknown <hans.cruz.0011@gmail.com>
2020-11-05 19:06:57 -0300

    implementando vistas

--- criticasHC/peliculas/urls.py
+++ criticasHC/peliculas/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views 
 urlpatterns=[
 	path('',views.index,name='index'),
-] 
+	path('criticas/',views.peliculas,name='pelis'),
+	path('quienes/',views.quienes,name='quienes'),
+	path('inicio/',views.inicio,name='inicio'),
+	path('registro/',views.registro,name='registro')
+	] 
--- criticasHC/peliculas/views.py
+++ criticasHC/peliculas/views.py
@@ -3,3 +3,13 @@ from .models import Movie
 # Create your views here.
 def index(request):
 	return render(request, 'index.html')
+def peliculas(request):
+	numpeli = Movie.objects.all() 
+	return render(request, 'pelis.html', context={"numpeli":numpeli} )
+def quienes(request):
+	return render(request, 'quienes.html')
+def inicio(request):
+	return render(request, 'inicio.html')
+def registro(request):
+	return render(request, 'registro.html')
+
